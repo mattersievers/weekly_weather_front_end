131834c17e052ae47ed458eb35fd23b2fda5a91d
Matthew Sievers <MatterSievers@gmail.com>
2022-04-25 00:43:43 -0500

    single day generates

--- src/components/DayWeather.js
+++ src/components/DayWeather.js
@@ -1,14 +1,24 @@
 function DayWeather(props){
-    
+    let {daily} = props.weatherData;    
+
+    //Gives the string for the day of the week when input Unix time from the weather API
+    let dayOfWeek = (day) => {
+        const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+        var dayNum = new Date(day.dt * 1000).getDay();
+        return weekdays[dayNum];
+    }
+
     if(!props){
         return (
             <div>Rendering...</div>
         )
     }
-    console.log(props)
+    console.log(daily)
     return (
         <div>
-            
+            <div className="day">{dayOfWeek(daily[0])}</div>
+            <img src={`http://openweathermap.org/img/wn/${daily[0].weather[0].icon}@2x.png`} />
+            <div className="max">{parseInt(daily[0].temp.max)} °  <span className="min">{parseInt(daily[0].temp.min)} ° </span></div>
         </div>
     )
 }
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,3 +1,7 @@
+//lat and lon are hardcoded for Austin. This can be changed
+const lat = '30.2671';
+const lon = '-97.7431';
+
 export const getWeather = () => {
-    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=30.2671&lon=-97.7431&exclude=minutely,hourly,alerts&appid=${process.env.REACT_APP_API_TOKEN}`);
+    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${process.env.REACT_APP_API_TOKEN}`);
 }
